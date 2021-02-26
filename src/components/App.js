1e9e72b2a4c61ae61179a45444893e196cf6db94
Nitrogen2Oxygen <nitrogenandoxygen@gmail.com>
2021-02-26 08:52:07 -0500

    Setup the react website

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import { Box } from '@chakra-ui/react';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <Box>
+      <Header />
+    </Box>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import { Box, Text } from '@chakra-ui/react';
+
+export default function Header() {
+    return (
+        <Box textAlign="center">
+      <Text fontSize="6xl">Web Timer</Text>
+    </Box>
+    )
+}
