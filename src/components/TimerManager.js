85e772cf8d0d7d4031486793b3908c63247554d9
Nitrogen2Oxygen <nitrogenandoxygen@gmail.com>
2021-02-28 16:32:23 -0500

    Added really jank controls

--- src/components/TimerManager.js
+++ src/components/TimerManager.js
@@ -6,11 +6,14 @@ export default function TimerManager() {
     const [time, setTime] = useState(0);
     const [timer, setTimer] = useState(false);
     const [notifs, setNotifs] = useState(false);
+    const [paused, setPaused] = useState(false);
 
     useEffect(() => {
         if (time > 0 && timer) {
-            // Countdown
-            setTimeout(setTime, 1000, time - 1);
+            if (!paused) {
+                // Countdown
+                setTimeout(setTime, 1000, time - 1);
+            }
         } else if (time > 0 && !timer) {
             // Reset
             setTime(0);
@@ -25,22 +28,72 @@ export default function TimerManager() {
         console.log(notifs)
     }, [notifs]);
 
+    useEffect(() => {
+        setTime(time - 1);
+    }, [paused]);  // eslint-disable-line react-hooks/exhaustive-deps
+
 
     return (
         <Box textAlign="center">
             <Timer time={time} />
+            <Box className="controls">
+                <PresetButton seconds={1 * 60} name="1 minute" />
+                <PresetButton seconds={5 * 60} name="5 minutes" />
+                <PresetButton seconds={10 * 60} name="10 minutes" />
+                <PresetButton seconds={30 * 60} name="30 minutes" />
+                <PresetButton seconds={60 * 60} name="1 hour" />
+            </Box>
+            <br />
+            <Box className="options">
+                <Button onClick={function() {
+                    setTimer(false);
+                    setTime(0);
+                }}>
+                    Reset
+                </Button>
+                <PauseContinueButton />
+                <NotificationButton notifs={notifs} />
+            </Box>
+        </Box>
+    )
+
+    // Buttons
+    function PresetButton(props) {
+        return (
             <Button onClick={function() {
                 if (timer) return;
                 setTimer(true)
-                setTime(10);
-            }}>Test</Button>
+                setTime(props.seconds);
+            }}>
+                {props.name}
+            </Button>
+        )
+    }
+
+    function PauseContinueButton() {
+        if  (!timer) return (
+            <Button>
+                Pause
+            </Button>
+        )
+        if (!paused) {
+            return (
             <Button onClick={function() {
-                setTimer(false);
-                setTime(0);
-            }}>Reset</Button>
-            <NotificationButton notifs={notifs} />
-        </Box>
-    )
+                setPaused(true);
+            }}>
+                Pause
+            </Button>
+            )
+        } else {
+            return (
+            <Button onClick={function() {
+                setPaused(false);
+            }}>
+                Continue
+            </Button>
+            )
+        }
+    }
 
     function NotificationButton(props) {
         var enabled = props.notifs;
@@ -69,5 +122,5 @@ export default function TimerManager() {
                 }}>Enable Notifications</Button>
                 ) 
         }
-    }    
+    }   
 }
